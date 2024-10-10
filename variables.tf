bb8c2d89bad7bf6b850031d2bed899a324ffcbfe
kumarkaji <153839042+kumarkaji@users.noreply.github.com>
2024-10-10 11:58:17 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,25 +4,20 @@ variable "access_key" {
 variable "secret_key" {
         description = "Secret key to AWS console"
 }
-
-
 variable "instance_name" {
         description = "Name of the instance to be created"
         default = "DEMO-instance"
 }
-
 variable "instance_type" {
         default = "t2.micro"
 }
-
 variable "subnet_id" {
         description = "The VPC subnet the instance(s) will be created in"
         default = "subnet-XXXXXXXXXXXXXXXX"
 }
-
 variable "ami_id" {
         description = "The AMI to use"
-        default = "ami-08e5424edfe926b43"
+        default = "ami-0866a3c8686eaeeba"
 }
 
 variable "number_of_instances" {
@@ -32,5 +27,5 @@ variable "number_of_instances" {
 
 
 variable "ami_key_pair_name" {
-        default = "Aditya"
+        default = "myawskey"
 }
